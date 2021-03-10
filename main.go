a455858d87a43b302749cfeae4a176bc719eed24
rishabh199 <63698663+rishabh199@users.noreply.github.com>
2021-03-10 11:12:20 +0530

    Update main.go

--- main.go
+++ main.go
@@ -9,4 +9,7 @@ func main() {
 	fmt.Printf("second change local")
 	fmt.Printf("Second change online")
 	fmt.Printf("Changes made and git working fine ")
+	fmt.Printf("Changes from online checking...... ")
+	fmt.Printf("Changes working fine.............. ")
+	
 }
