b2551c1cf03c693e7d12b3db7e4c10c108f162e7
rishabh <rishabhgupta99779@gmail.com>
2021-06-01 10:50:22 +0530

    new

--- change.go
+++ change.go
@@ -3,5 +3,6 @@ package main
 import "fmt"
 
 func main() {
-  fmt.Printf("Trying new file with new changes ")
+	fmt.Printf("Trying new file with new changes ")
+	fmt.Printf("Trying new file with new change 2")
 }
