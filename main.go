40dced81d73ab35c200d8b8dc2b90eb5978cf4fb
rishabh <rishabhgupta99779@gmail.com>
2021-06-01 10:49:49 +0530

    new changes

--- main.go
+++ main.go
@@ -11,5 +11,6 @@ func main() {
 	fmt.Printf("Changes made and git working fine ")
 	fmt.Printf("Changes from online checking...... ")
 	fmt.Printf("Changes working fine.............. ")
-	
+	fmt.Printf("Trying new file with new changes ")
+
 }
